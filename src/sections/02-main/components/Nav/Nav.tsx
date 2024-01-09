75e7602eb63a20a5cf1d24f8e23a5246a90e75ba
jajablinky <gmillar6@gmail.com>
2024-01-08 16:26:10 -0800

    edited readme

--- src/sections/02-main/components/Nav/Nav.tsx
+++ src/sections/02-main/components/Nav/Nav.tsx
@@ -1,10 +1,15 @@
 import './Nav.css';
+import aoFont from '../../../../assets/ao-font.svg';
+import aoPict from '../../../../assets/ao-pictograph.svg';
 
 const Nav = () => {
   return (
     <header>
       <nav>
-        <p>Logo</p>
+        <div className="ao-wrapper">
+          <img src={aoPict} alt="ao-pictograph" />
+          <img src={aoFont} alt="ao-font" />
+        </div>
         <div className="nav-buttons">
           <button>Docs</button>
           <button>Specs</button>
