6f99a3cfcfef0758420b020561b59827adf62cf5
jajablinky <gmillar6@gmail.com>
2024-01-11 16:40:44 -0800

    hack font

--- src/routes/home/sections/02-main/Main.tsx
+++ src/routes/home/sections/02-main/Main.tsx
@@ -2,6 +2,7 @@ import './MainStyles.css';
 import { useEffect } from 'react';
 import morpheusAsciiArt from '../../../../components/MorpheusAsciiArt.tsx';
 import aoMorphingShape from '../../../../assets/ao.mp4';
+import HyperTextLoad from '../../../../components/hyperTextLoad.tsx';
 
 const Main = () => {
   useEffect(() => {
@@ -12,9 +13,40 @@ const Main = () => {
     <>
       <main>
         <div className="home-main-wrapper">
-          <video height={1000} autoPlay loop muted>
+          <video height={300} autoPlay loop muted>
             <source src={aoMorphingShape} type="video/mp4" />
           </video>
+          <div className="text-hero-wrapper">
+            <div className="main-heading">
+              <HyperTextLoad
+                word={'Hyper.'}
+                textType={'h1'}
+                speed={1}
+                triggerOnLoad
+              />
+              <HyperTextLoad
+                word={'Parallel.'}
+                textType={'h1'}
+                speed={2}
+                triggerOnLoad
+              />
+              <HyperTextLoad
+                word={'Compute.'}
+                textType={'h1'}
+                speed={3}
+                triggerOnLoad
+              />
+            </div>
+            <div className="button-wrapper">
+              <button className="glitch primary" data-text="Boot Up Testnet">
+                <HyperTextLoad
+                  word={'Boot Up TestNet'}
+                  textType="span"
+                  speed={1}
+                />
+              </button>
+            </div>
+          </div>
         </div>
       </main>
     </>
