6ab29e07c8945e6ef1b62c249b5cdf72d8d4a1d1
jajablinky <gmillar6@gmail.com>
2024-01-24 11:20:27 -0800

    created scan route

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Spec from './routes/spec/Spec';
 import Footer from './components/Footer/Footer';
 import { useEffect } from 'react';
 import { setColorScheme } from './components/HelperFunctions/setColorScheme';
+import Scan from './routes/scan/Scan';
 
 function App() {
   useEffect(() => {
@@ -26,6 +27,7 @@ function App() {
       <Routes>
         <Route path={'/'} element={<Home />} />
         <Route path={'/spec'} element={<Spec />} />
+        <Route path={'/scan'} element={<Scan />} />
       </Routes>
       <Footer />
     </HashRouter>
