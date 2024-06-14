1d95c43b6915bbfed0f203a960c2ee84823207cf
Daniel Constantin <hello@danielconstantin.net>
2024-06-14 23:09:32 +0300

    Use blocknative widget for Ethereum wallet connection

--- src/providers/EthereumProvider.tsx
+++ src/providers/EthereumProvider.tsx
@@ -1,13 +1,74 @@
-import React from 'react';
+import React, { createContext, useCallback, useContext, useEffect, useState } from 'react';
 import { Web3Provider } from '@ethersproject/providers';
 import { formatEther } from '@ethersproject/units';
+import coinbaseWalletModule from '@web3-onboard/coinbase';
+import Onboard from '@web3-onboard/core';
+// import gnosisSafeModule from '@web3-onboard/gnosis';
+import injectedModule from '@web3-onboard/injected-wallets';
+// import portisModule from '@web3-onboard/portis';
+import torusModule from '@web3-onboard/torus';
+// import trezorModule from '@web3-onboard/trezor';
+import trustModule from '@web3-onboard/trust';
+// import walletConnectModule from '@web3-onboard/walletconnect';
 
-// import WalletConnectProvider from '@walletconnect/web3-provider';
-import { Modal } from 'components/molecules/Modal';
-import { ETH_WALLETS } from 'helpers/config';
+const injected = injectedModule();
+const trust = trustModule();
+const torus = torusModule();
+const coinbaseWalletSdk = coinbaseWalletModule();
+// const walletConnect = walletConnectModule({
+// 	projectId: '<PROJECT_ID>', // TODO
+// });
+// const trezor = trezorModule({
+// 	email: '<EMAIL_CONTACT>', // TODO
+// 	appUrl: '<APP_URL>', // TODO
+// });
+// const gnosisSafe = gnosisSafeModule({
+// 	whitelistedDomains: [new RegExp('^http://localhost:3000$')], // TODO
+// });
+// const portis = portisModule({
+// 	apiKey: '<API_KEY>', // TODO
+// });
 
-import { useLanguageProvider } from './LanguageProvider';
-import * as S from './styles';
+const wallets = [
+	injected,
+	trust,
+	coinbaseWalletSdk,
+	torus,
+	// walletConnect,
+	// trezor,
+	// gnosisSafe,
+	// portis,
+];
+
+const onboard = Onboard({
+	wallets,
+	chains: [
+		{
+			id: '0x1', // Chain ID in hexadecimal for Ethereum Mainnet
+			token: 'ETH',
+			label: 'Ethereum Mainnet',
+			// rpcUrl: 'https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID', // Replace with your Infura project ID or use a public RPC URL
+		},
+	],
+	appMetadata: {
+		name: 'AO',
+		icon: '/images/ao_black.svg',
+		logo: '/images/ao_black.svg',
+		description: 'AO Staking',
+	},
+	connect: {
+		autoConnectLastWallet: true,
+	},
+	accountCenter: {
+		desktop: {
+			enabled: false,
+		},
+		mobile: {
+			enabled: false,
+		},
+	},
+});
+
 interface EthereumContextState {
 	walletAddress: string | null;
 	balance: string | null;
@@ -32,115 +93,82 @@ const DEFAULT_CONTEXT: EthereumContextState = {
 	errorMessage: null,
 };
 
-const EthereumContext = React.createContext<EthereumContextState>(DEFAULT_CONTEXT);
+const EthereumContext = createContext<EthereumContextState>(DEFAULT_CONTEXT);
 
-export const useEthereumProvider = () => React.useContext(EthereumContext);
-
-function WalletList(props: { handleConnect: any }) {
-	// const isWalletInstalled = (walletType: string) => {
-	// 	switch (walletType) {
-	// 		case 'MetaMask':
-	// 			return !!window.ethereum;
-	// 		case 'WalletConnect':
-	// 			return true;
-	// 		case 'Rabby':
-	// 			return !!window.ethereum?.isRabby;
-	// 		case 'Phantom':
-	// 			return !!window.solana?.isPhantom;
-	// 		default:
-	// 			return false;
-	// 	}
-	// };
-
-	return (
-		<S.WalletListContainer>
-			{ETH_WALLETS.map((wallet: any, index: number) => (
-				<S.WalletListItem
-					key={index}
-					onClick={() => props.handleConnect(wallet.type)}
-					className={'border-wrapper-alt2'}
-				>
-					{wallet.logo && <img src={`${wallet.logo}`} alt={''} />}
-					<span>{wallet.type.charAt(0).toUpperCase() + wallet.type.slice(1)}</span>
-					{/* {!isWalletInstalled(wallet.type) && <div style={{ height: 10, width: 10, background: 'red' }} />} */}
-				</S.WalletListItem>
-			))}
-		</S.WalletListContainer>
-	);
-}
+export const useEthereumProvider = () => useContext(EthereumContext);
 
 export function EthereumProvider({ children }: EthereumProviderProps) {
-	const languageProvider = useLanguageProvider();
-	const language = languageProvider.object[languageProvider.current];
+	const [walletAddress, setWalletAddress] = useState<string | null>(null);
+	const [balance, setBalance] = useState<string | null>(null);
+	const [walletModalVisible, setWalletModalVisible] = useState<boolean>(false);
+	const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
-	const [walletAddress, setWalletAddress] = React.useState<string | null>(null);
-	const [balance, setBalance] = React.useState<string | null>(null);
-	const [walletModalVisible, setWalletModalVisible] = React.useState<boolean>(false);
-	const [errorMessage, setErrorMessage] = React.useState<string | null>(null);
+	const recoverConnection = useCallback(async () => {
+		const [primaryWallet] = onboard.state.get().wallets;
+		if (primaryWallet) {
+			const success = await onboard.setChain({ chainId: '0x1' });
+			if (!success) return;
 
-	const handleConnect = async (walletType: string) => {
+			const provider = new Web3Provider(primaryWallet.provider);
+			const signer = provider.getSigner();
+			const address = await signer.getAddress();
+			setWalletAddress(address);
+
+			const balance = await signer.getBalance();
+			setBalance(formatEther(balance));
+		}
+	}, []);
+
+	useEffect(() => {
+		recoverConnection();
+	}, []);
+
+	const handleConnect = async () => {
 		try {
-			let provider: any;
-			switch (walletType) {
-				case 'MetaMask':
-					if (window.ethereum) {
-						provider = new Web3Provider(window.ethereum);
-						await provider.send('eth_requestAccounts', []);
-					} else {
-						alert('MetaMask is not installed');
-					}
-					break;
-				case 'Rabby':
-					if (window.ethereum?.isRabby) {
-						provider = new Web3Provider(window.ethereum);
-						await provider.send('eth_requestAccounts', []);
-					} else {
-						alert('Rabby is not installed');
-					}
-					break;
-				case 'Phantom':
-					if (window.solana?.isPhantom) {
-						await window.solana.connect();
-						provider = new Web3Provider(window.solana);
-					} else {
-						alert('Phantom is not installed');
-					}
-					break;
-				default:
-					alert('Unsupported wallet type');
-			}
-			
-			const network = await provider.getNetwork();
-			if (network.name === 'homestead') {
-				const signer = provider.getSigner();
-				const address = await signer.getAddress();
-				setWalletAddress(address);
-	
-				const balance = await signer.getBalance();
-				setBalance(formatEther(balance));
-				setWalletModalVisible(false);
-			}
-			else {
-				alert('Your wallet must be connected to ETH Mainnet!')
-			}
+			const [primaryWallet] = await onboard.connectWallet();
+			if (!primaryWallet) throw new Error('No wallet selected');
+
+			const success = await onboard.setChain({ chainId: '0x1' });
+			if (!success) throw new Error('Please switch to Ethereum Mainnet');
+
+			const provider = new Web3Provider(primaryWallet.provider);
+			const signer = provider.getSigner();
+			const address = await signer.getAddress();
+			setWalletAddress(address);
+
+			const balance = await signer.getBalance();
+			setBalance(formatEther(balance));
+
+			setWalletModalVisible(false);
 		} catch (error) {
 			setErrorMessage(error.message);
 			setWalletAddress(null);
+			setBalance(null);
+		}
+
+		setWalletModalVisible(false);
+	};
+
+	const handleDisconnect = async () => {
+		const [primaryWallet] = onboard.state.get().wallets;
+		if (primaryWallet) {
+			try {
+				await onboard.disconnectWallet({ label: primaryWallet.label });
+			} finally {
+				setWalletAddress(null);
+				setBalance(null);
+			}
 		}
 	};
 
-	const handleDisconnect = () => {
-		setWalletAddress(null);
-		setBalance(null);
-	};
+	useEffect(() => {
+		if (walletModalVisible) {
+			handleConnect();
+		}
+	}, [walletModalVisible]);
 
 	return (
 		<>
-			{walletModalVisible && (
-				<Modal header={language.connectWallet} handleClose={() => setWalletModalVisible(false)}>
-					<WalletList handleConnect={handleConnect} />
-				</Modal>
-			)}
 			<EthereumContext.Provider
 				value={{
 					walletAddress,
