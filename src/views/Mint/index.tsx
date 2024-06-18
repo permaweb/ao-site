e3625087d55a467fdd5310afe77ee6a4f7504b7b
NickJ202 <nickjuliano20@gmail.com>
2024-06-18 17:21:43 -0400

    fix header positioning

--- src/views/Mint/index.tsx
+++ src/views/Mint/index.tsx
@@ -17,23 +17,23 @@ export default function Mint() {
 	const [isBlocked, setIsBlocked] = React.useState<boolean>(false);
 
 	// TODO
-	// React.useEffect(() => {
-	// 	const checkLocation = async () => {
-	// 		setLoading(true);
-	// 		try {
-	// 			const response = await fetch(ENDPOINTS.ipCheck);
-	// 			const data = await response.json();
-	// 			if (data.country === 'US') {
-	// 				setIsBlocked(true);
-	// 			}
-	// 		} catch (error) {
-	// 			console.error('Error fetching location data', error);
-	// 		}
-	// 		setLoading(false);
-	// 	};
+	React.useEffect(() => {
+		const checkLocation = async () => {
+			setLoading(true);
+			try {
+				const response = await fetch(ENDPOINTS.ipCheck);
+				const data = await response.json();
+				if (data.country === 'US') {
+					setIsBlocked(true);
+				}
+			} catch (error) {
+				console.error('Error fetching location data', error);
+			}
+			setLoading(false);
+		};
 
-	// 	checkLocation();
-	// }, []);
+		checkLocation();
+	}, []);
 
 	function getView() {
 		if (loading) return <Loader />;
