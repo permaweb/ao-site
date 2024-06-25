7874b226777116895664d29629892ab588a9a8b0
NickJ202 <nickjuliano20@gmail.com>
2024-06-25 13:22:30 -0400

    update token mirror

--- src/helpers/config.ts
+++ src/helpers/config.ts
@@ -19,12 +19,9 @@ import walletConnect from 'assets/wallet-connect.png';
 
 import { ArWalletEnum, EthWalletEnum } from './types';
 
-// TODO
-// Mirror: F-EvpwmZXIlndrEqXOXSSifUeyn-LMBdeJKI6Gflk1g
-// Prod: m3PaWzK4PTG9lAaqYQPaPdOcXdO8hYqi5Fe9NWqXd0w
 export const AO = {
 	token: 'm3PaWzK4PTG9lAaqYQPaPdOcXdO8hYqi5Fe9NWqXd0w',
-	tokenMirror: 'F-EvpwmZXIlndrEqXOXSSifUeyn-LMBdeJKI6Gflk1g',
+	tokenMirror: 'Pi-WmAQp2-mh-oWH9lWpz5EthlUDj_W0IusAv-RXhRk',
 	cred: 'Sa0iBLPNyJQrwpTTG-tWLQU-1QeUAJA73DdxGGiKoJc',
 	aoClaim: 'U2Bv-LEoFzwAFfBx9MiXNnAfaYRjT4MG9T7sFcVHn20',
 };
