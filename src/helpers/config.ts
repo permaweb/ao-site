3397eab66427a87b7ecfb9911751d5980fae0755
smowden <smowden@users.noreply.github.com>
2024-08-27 15:24:09 +0300

    keep staging mirror for now

--- src/helpers/config.ts
+++ src/helpers/config.ts
@@ -26,7 +26,7 @@ import { ArWalletEnum, EthWalletEnum } from './types';
 
 export const AO = {
 	token: 'm3PaWzK4PTG9lAaqYQPaPdOcXdO8hYqi5Fe9NWqXd0w',
-	tokenMirror: 'ptCu-Un-3FF8sZ5zNMYg43zRgSYAGVkjz2Lb0HZmx2M',
+	tokenMirror: 'yeoVbmATBB951KP0dU2qRUIhtpUFt1lnZ4UKpSOL5dw',
 	cred: 'Sa0iBLPNyJQrwpTTG-tWLQU-1QeUAJA73DdxGGiKoJc',
 	aoClaim: 'U2Bv-LEoFzwAFfBx9MiXNnAfaYRjT4MG9T7sFcVHn20',
 	stEthPriceOracle: 'rwxd1EuSzEVoy3qTlDRm7eMYPbOMrmQMajynVrA-ikk',
